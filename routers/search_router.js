b8631c46b7f68ad5a17fc320f904605e14125581
Emily Lordahl <contact@emilylordahl.com>
2015-04-19 15:04:23 -0400

    request experimentation, async sucks

--- routers/search_router.js
+++ routers/search_router.js
@@ -7,28 +7,37 @@ router.use(logger('dev'));
 
 router.get('/', function (req, res) {
 
-	// Get the current user ID, make another request to the current user path, get all the other data, then ccreate the query string.
+	var age      = 25;
+	var gender   = 'female';
+	var pregnant = false;
+	var baseUrl  = 'http://healthfinder.gov/developer/MyHFSearch.json?api_key=';
+	var apiKey   = 'xdwpkcqluwfuahrx';
+
 	request({
-		uri: '/current_user',
-		method: 'GET'
+		uri: baseUrl + apiKey +	'&age=' + age + '&gender=' + gender + '&pregnant=' + pregnant,
+		method: 'GET',
+		json: true
 	}, function (error, response, body) {
-			var age      = body.age;
-			var gender   = body.gender;
-			var pregnant = body.pregnant;
-			var baseUrl  = 'http://healthfinder.gov/developer/MyHFSearch.json?api_key=';
-			var apiKey   = 'xdwpkcqluwfuahrx';
-
-			request({
-				uri: baseUrl + apiKey +	'&age=' + age + '&gender=' + gender + '&pregnant=' + pregnant,
-				method: 'GET'
-			}, function (error, response, body) {
-
-			});
+		var results = body.Result.Topics;
+		results.forEach(function(title) {
+			var resultData = title.Title;
+		console.log(resultData);	
+		return resultData;
+		res.send(resultData);
+		});
 	});
 });
 
+
 module.exports = router;
 
+	// Get the current user ID, make another request to the current user path, get all the other data, then ccreate the query string.
+	// request({
+	// 	uri: '/current_user',
+	// 	method: 'GET'
+	// }, function (error, response, body) {
+
+// });
 
 // Can I do this instead of the above for the nested request at line 14?
 // function (error, response, body) {
@@ -46,4 +55,4 @@ module.exports = router;
 // 			}, function (error, response, body) {
 
 // 			});
-// 	});
+	// });
