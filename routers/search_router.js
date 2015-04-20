23558b8cffba9776121ed7c66ae4de624e5f43e7
Emily Lordahl <contact@emilylordahl.com>
2015-04-19 20:35:07 -0400

    request node set header issue not resolved

--- routers/search_router.js
+++ routers/search_router.js
@@ -7,31 +7,33 @@ router.use(logger('dev'));
 
 router.get('/', function (req, res) {
 
-	// Need to first make a request to the server to /current_user and get the id
+	// After a user has signed it, need to first make a request to the server to /current_user and get that user's id
 	// Then ping the server again for /users/:id to get the age, gender and pregnancy status
-	// Then use all that information to create the query string
+	// Then use all that information to create the query string below
 
 	request({
 		uri: 'http://healthfinder.gov/developer/MyHFSearch.json?api_key=',
 		method: 'GET',
 		qs: {
 			api_key: 'xdwpkcqluwfuahrx',
-			age: 25,
-			gender: 'female',
-			pregnant: false
+			age: 25, // This will be user.age
+			gender: 'female', // This will be user.gender
+			pregnant: false // This will be user.pregnant or default of false
 		},
 		json: true
 	}, function (error, response, body) {
 		var results = body.Result.Topics;
+		// console.log(results);
 		results.forEach(function(title) {
-			var resultData = title.Title;
-		console.log(resultData);	
-		return resultData;
+			var resultData = {
+				tip: title.Title,
+				description: title.MyHFDescription
+			};
+		console.log(resultData);
 		res.send(resultData);
 		});
 	});
 });
-
 
 module.exports = router;
 
