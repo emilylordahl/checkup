2ffc8f83fc453f6d3a30608bd447c273a3ca731c
Emily Lordahl <contact@emilylordahl.com>
2015-04-21 13:44:18 -0400

    login with successful template render

--- public/js/views/login.js
+++ public/js/views/login.js
@@ -1,23 +1,54 @@
 App.Views.Login = Backbone.View.extend({
 
-	el: '#login',
+	el: 'body',
 
 	initialize: function() {
 		console.log('Created: Login View');
+		loginTemplate = Handlebars.compile($('#login-template').html());
+		loggedInTemplate = Handlebars.compile($('#logged-in-template').html());
+		this.renderSession();
 	},
 
 	events: {
-		'click #login-button' : 'login'
+		'click #login-button' : 'login',
+		'click #logout-button': 'logout'
+	},
+
+	renderSession: function() {
+		$.get('/current_user').done(function(user) {
+			if (user) {
+				$('#session').html(loggedInTemplate(user));
+			} else {
+				$('#session').html(loginTemplate());
+			}
+		}).fail(function(jqXHR) {
+			if (jqXHR.status === 404) {
+				$('#session').html('404 Not Found :(');
+			}
+		});
 	},
 
 	login: function() {
-		console.log('You clicked the signup button!');
+		console.log('You clicked the login button!');
 		var username  = $('[name="login-username"]').val();
 		var password  = $('[name="login-password"]').val();
 
 		$.post('/sessions', {
 			username: username,
 			password: password
-		}).done();
+		}).done(this.renderSession);
+
+		$('#signup').hide();
+	},
+
+	logout: function() {
+		console.log('You clicked the logout button!');
+		$.ajax({
+			url: '/sessions',
+			method: 'DELETE'
+		}).done(this.renderSession);
+
+		$('#signup').show();
+
 	}
 });
